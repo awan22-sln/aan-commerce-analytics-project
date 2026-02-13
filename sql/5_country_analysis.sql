956c050e2b9d08e8c18ebd55b10b70d1910dbe26
awan22-sln <aansetiyawan22@gmail.com>
2026-02-13 09:45:22 +0700

    Create 5_country_analysis.sql

---
+++ sql/5_country_analysis.sql
@@ -0,0 +1,16 @@
+SELECT
+  geoNetwork.country AS country,
+  COUNT(DISTINCT fullVisitorId) AS users,
+  SUM(totals.transactions) AS transactions,
+  SUM(totals.transactionRevenue) / 1000000 AS revenue
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  _TABLE_SUFFIX BETWEEN '20160801' AND '20160831'
+GROUP BY
+  country
+HAVING
+  revenue IS NOT NULL
+ORDER BY
+  revenue DESC
+LIMIT 20;
